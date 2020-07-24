b5d5ccfa987b953db4ef62f0857ffef8454b0111
Ítalo Silveira <italobrian@gmail.com>
2020-07-23 23:51:53 -0300

    Update index.js

--- src/generator-bs-dotnetcore/generators/app/index.js
+++ src/generator-bs-dotnetcore/generators/app/index.js
@@ -2,7 +2,27 @@
 const Generator = require("yeoman-generator");
 const chalk = require("chalk");
 const yosay = require("yosay");
-
+const constants = {
+  projectTypes: {
+    DDDFLIB: "DDDFLIB",
+    DDDFAPP: "DDDFAPP",
+    DDDFAPI: "DDDFAPI",
+    DDDLLIB: "DDDLLIB",
+    DDDLAPP: "DDDLAPP",
+    DDDLAPI: "DDDLAPI",
+    CQRSFLIB: "CQRSFLIB",
+    CQRSFAPP: "CQRSFAPP",
+    CQRSFAPI: "CQRSFAPI",
+    CQRSLLIB: "CQRSLLIB",
+    CQRSLAPP: "CQRSLAPP",
+    CQRSLAPI: "CQRSLAPI"
+  },
+  codeChunkTypes: {
+    ADDPROJECT: "ADDPROJECT",
+    ADDCONTEXT: "ADDCONTEXT",
+    ADDENTITYFLOW: "ADDENTITYFLOW"
+  }
+};
 module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
@@ -17,56 +37,75 @@ module.exports = class extends Generator {
     const prompts = [
       {
         type: "list",
-        name: "project",
+        name: "projectType",
         message: "Select project type:",
         choices: [
           {
             name: "DDD - FullLIB",
-            value: "DDDFLIB"
+            value: constants.projectTypes.DDDFLIB
           },
           {
             name: "DDD - FullAPP",
-            value: "DDDFAPP"
+            value: constants.projectTypes.DDDFAPP
           },
           {
             name: "DDD - FullAPI",
-            value: "DDDFAPI"
+            value: constants.projectTypes.DDDFAPI
           },
           {
             name: "DDD - LiteLIB",
-            value: "DDDFLIB"
+            value: constants.projectTypes.DDDLLIB
           },
           {
             name: "DDD - LiteAPP",
-            value: "DDDFAPP"
+            value: constants.projectTypes.DDDLAPP
           },
           {
             name: "DDD - LiteAPI",
-            value: "DDDFAPI"
+            value: constants.projectTypes.DDDLAPI
           },
           {
             name: "CQRS - FullLIB",
-            value: "CQRSFLIB"
+            value: constants.projectTypes.CQRSFLIB
           },
           {
             name: "CQRS - FullAPP",
-            value: "CQRSFAPP"
+            value: constants.projectTypes.CQRSFAPP
           },
           {
             name: "CQRS - FullAPI",
-            value: "CQRSFAPI"
+            value: constants.projectTypes.CQRSFAPI
           },
           {
             name: "CQRS - LiteLIB",
-            value: "CQRSLLIB"
+            value: constants.projectTypes.CQRSLLIB
           },
           {
             name: "CQRS - LiteAPP",
-            value: "CQRSLAPP"
+            value: constants.projectTypes.CQRSLAPP
           },
           {
             name: "CQRS - LiteAPI",
-            value: "CQRSLAPI"
+            value: constants.projectTypes.CQRSLAPI
+          }
+        ]
+      },
+      {
+        type: "list",
+        name: "codeChunkType",
+        message: "Select code chunk type:",
+        choices: [
+          {
+            name: "Add project",
+            value: constants.codeChunkTypes.ADDPROJECT
+          },
+          {
+            name: "Add context",
+            value: constants.codeChunkTypes.ADDCONTEXT
+          },
+          {
+            name: "Add entity flow",
+            value: constants.codeChunkTypes.ADDENTITYFLOW
           }
         ]
       }
@@ -78,8 +117,28 @@ module.exports = class extends Generator {
     });
   }
 
+  addProject() {
+    if (this.props.codeChunkType === constants.codeChunkTypes.ADDPROJECT) {
+      this.log("This method adds a project code chunk.");
+    }
+  }
+
+  addContext() {
+    if (this.props.codeChunkType === constants.codeChunkTypes.ADDCONTEXT) {
+      this.log("This method adds a context code chunk.");
+    }
+  }
+
+  addEntityFlow() {
+    if (this.props.codeChunkType === constants.codeChunkTypes.ADDENTITYFLOW) {
+      this.log("This method adds a entity flow code chunk.");
+    }
+  }
+
   writing() {
-    this.log("Project: ", this.props.project);
+    this.log("Project type: ", this.props.projectType);
+
+    this.log("Code chunk: ", this.props.codeChunkType);
 
     this.fs.copy(
       this.templatePath("dummyfile.txt"),
